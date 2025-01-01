5a94e5613bba239c5bd27804c3ea485e6ee7f1f2
Aryat <aryatsrishadow007@gmail.com>
2025-01-01 12:17:11 +0530

    Initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,15 @@
 import React from "react";
-import Header from './header' // Import Header component
-import Post from "./post"; // Import Post component
-import "./App.css"; // Import CSS file
+import Header from "./header"; 
+import Post from './post'
+import "./App.css"; 
 
 function App() {
   return (
     <main>
       {/* Header Section */}
-<<<<<<< HEAD
       <Header />
-      Blog Post section
-=======
-      <Header /> 
->>>>>>> 803502e68af56826b7da1512845c73cb096dd207
-      
-      {/* Blog Entry Section */}
+      Blog Post section :{/* Blog Entry Section */}
       <div className="posts-container">
-        
         <Post /> {/* Add more Post components if needed */}
       </div>
     </main>
